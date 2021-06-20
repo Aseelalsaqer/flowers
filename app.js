713aa72b6b59d9f57ce6006d6e9c7d8db240af32
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-06-20 16:06:48 +0300

    lab01b

---
+++ app.js
@@ -0,0 +1,15 @@
+let FavouritFlower = prompt("What is your favourit flower?")
+console.log(FavouritFlower)
+alert('Good Taste')
+
+let flowerrating = prompt("On a scale of 10 , how mush do you like Flowers?")
+console.log(flowerrating)
+alert('I like Flowers Alot too')
+
+let flowernumber = prompt("Guess How many kinds of flowers is in the world!")
+console.log(flowernumber)
+alert('30000, yes very big number')
+
+let rating = prompt("was this article useful?")
+console.log(rating)
+alert('Thank You for your time')
